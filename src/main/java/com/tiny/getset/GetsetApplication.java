cb39cbd3351a2807507302697160fa2391822642
tinyg <tinygone@qq.com>
2020-02-02 11:58:19 +0800

    init

---
+++ src/main/java/com/tiny/getset/GetsetApplication.java
@@ -0,0 +1,13 @@
+package com.tiny.getset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetsetApplication.class, args);
+	}
+
+}

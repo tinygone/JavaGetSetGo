ca3b96a167f396676e84421ff1d5ba20a374c8fb
tinyg <tinygone@qq.com>
2020-02-02 12:00:00 +0800

    init

---
+++ src/test/java/com/tiny/getset/GetsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiny.getset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
